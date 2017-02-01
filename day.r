4c03e66f6acb8c72e325ce8fcf1315cfd3b6a240
Denis Rasulev <ranklord@gmail.com>
2017-02-01 16:55:15 +0100

    daily values function

--- day.r
+++ day.r
@@ -1,39 +1,74 @@
-# day by hour
+# functions to extract meter readings for selected meter, day, month and year
+# © 2017 Denis Rasulev - ranklord@gmail.com
 
 # set working directory
 setwd("/Volumes/data/projects/energyhack")
 
-# set variables
-meter <- 662
-day   <- 1
-month <- 1
-year  <- 2016
+# meter readings by 15 minutes intervals
+day_by_min <- function(meter = 1, day = 1, month = 1, year = 2016) {
 
-# read data for required meter
-path <- paste("data/2016_maf_", meter, ".rds", sep = "")
-data <- readRDS(path)
+    # validate arguments
+    if (meter < 1 | meter > 1000) {
+        stop("Meter number must be between 1 and 1000", call. = FALSE)
+    }
+    if (day < 1 | day > 31) {
+        stop("Day must be between 1 and 31", call. = FALSE)
+    }
+    if (month < 1 | month > 12) {
+        stop("Month must be between 1 and 12", call. = FALSE)
+    }
+    if (year < 2016 | year > 2017) {
+        stop("Year must be 2016 or 2017", call. = FALSE)
+    }
 
-# extract values according to required variables
-newdata <- data[ which(  data$measurements.meterID == meter &
-                         data$measurements.day     == day &
-                         data$measurements.month   == month &
-                         data$measurements.year    == year), "measurements.consumption" ]
-newdata <- as.data.frame(newdata)
-colnames(newdata) <- "cons"
+    # read data for required meter
+    path <- paste("data/2016_maf_", meter, ".rds", sep = "")
+    data <- readRDS(path)
 
-z    <- 1
-hour <- 0
-for (i in seq(1, nrow(newdata), 4)) {
-     hour[z] <- sum(newdata[i:(i + 3), ])
-     z <- z + 1
+    # reduce meter data by subsetting to only required day, month and year
+    data <- data[   data$measurements.day == day &
+                    data$measurements.month == month &
+                    data$measurements.year == year, ]$measurements.consumption
+
+    # return values
+    return(data[[1]])
 }
 
-# plot
-par(las = 1, mar = c(3, 4, 3, 1) + .1)
-barplot(hour,
-        main = paste("Consumption per Month", month, "Day", day, "by hour"),
-        xlab = paste("Meter", meter, "- Total consumption per this day:", sum(hour), "kWh"),
-        yaxp = c(0, round(max(hour)), 5),
-        col = "aquamarine",
-        cex.names = .5,
-        names.arg = c(1:length(hour)))
+# all 15 minutes readings are sumed by 4 to return hourly values
+day_by_hour <- function(meter = 1, day = 1, month = 1, year = 2016) {
+
+    # validate arguments
+    if (meter < 1 | meter > 1000) {
+        stop("Meter number must be between 1 and 1000", call. = FALSE)
+    }
+    if (day < 1 | day > 31) {
+        stop("Day must be between 1 and 31", call. = FALSE)
+    }
+    if (month < 1 | month > 12) {
+        stop("Month must be between 1 and 12", call. = FALSE)
+    }
+    if (year < 2016 | year > 2017) {
+        stop("Year must be 2016 or 2017", call. = FALSE)
+    }
+
+    # read data for required meter
+    path <- paste("data/2016_maf_", meter, ".rds", sep = "")
+    data <- readRDS(path)
+
+    # reduce meter data by subsetting to only required day, month and year
+    data <- data[   data$measurements.day   == day &
+                    data$measurements.month == month &
+                    data$measurements.year  == year, ]$measurements.consumption
+
+    # sum all meter readings by hour (1:4 for hour 0, 5:8 for hour 1, etc)
+    # since meter saves readings once every 15 minutes we need to sum by 4
+    j    <- 1
+    hour <- 0
+    for (i in seq(1, length(unlist(data)), 4)) {
+        hour[j] <- sum( data[[1]][i:(i + 3)] )
+        j <- j + 1
+    }
+
+    # return values
+    return(hour)
+}
